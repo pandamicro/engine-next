60ae47e72ea00f1125c0fda633f95a7d1c176ce2
pandamicro <lphuabin@gmail.com>
2017-08-01 11:10:34 +0800

    Init

---
+++ index.js
@@ -0,0 +1,15 @@
+// intenral
+
+
+// deps
+import * as math from 'vmath';
+import renderer from 'renderer.js';
+import gfx from 'gfx.js';
+
+let engine = {
+  math,
+  renderer,
+  gfx
+};
+
+export default engine;
---
+++ script/rollup.config.js
@@ -0,0 +1,39 @@
+'use strict';
+
+const fsJetpack = require('fs-jetpack');
+const pjson = require('../package.json');
+const resolve = require('rollup-plugin-node-resolve');
+
+let banner = `
+/*
+ * ${pjson.name} v${pjson.version}
+ * (c) ${new Date().getFullYear()} @pandamicro
+ * Released under the MIT License.
+ */
+`;
+
+let dest = './dist';
+let file = 'engine';
+let moduleName = 'engine';
+
+// clear directory
+fsJetpack.dir(dest, { empty: true });
+
+module.exports = {
+  entry: './index.js',
+  targets: [
+    { dest: `${dest}/${file}.dev.js`, format: 'iife' },
+    { dest: `${dest}/${file}.js`, format: 'cjs' },
+  ],
+  moduleName,
+  banner,
+  external: [],
+  globals: {},
+  sourceMap: true,
+  plugins: [
+    resolve({
+      jsnext: true,
+      main: true,
+    })
+  ],
+};
